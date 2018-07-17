5e4dee9dbf4f3e9f26a077bfa3ca78c89920efaa
Yvan Douenel <y.douenel@coopernet.fr>
2018-07-17 18:27:09 +0200

    checktopscroll up to manage slideshow.

---
+++ web/themes/contrib/ensemble/less/js/checktopscroll/checktopscroll.js
@@ -0,0 +1,30 @@
+(function ($) {
+  var scrolled = false;
+  $(window).scroll(function() {
+    clearTimeout($.data(this, 'scrollTimer'));
+    $.data(this, 'scrollTimer', setTimeout(function() {
+      //console.log("Haven't scrolled in 250ms!");
+      if($(window).scrollTop() && !scrolled) {
+        $(".logo img").animate({
+          height: "60px"
+        }, 500, function() {
+          // Animation complete.
+        });
+        scrolled = true;
+
+      } else if (scrolled && $(window).scrollTop() == 0) {
+        $("#full-content-header").css("margin-top","15px");
+        $(".logo img").animate({
+          height: "130px"
+        }, 500, function() {
+          // Animation complete.
+        });
+
+        scrolled = false;
+      }
+
+    }, 250));
+  });
+
+
+})(jQuery);
