d02ceb1dcf45ee0b6de9080eca20900ac6b5febb
Yvan Douenel <y.douenel@coopernet.fr>
2018-07-23 10:56:03 +0200

    incentive.js added and ensemble.theme up to call it.

--- web/themes/contrib/ensemble/less/js/incentive/incentive.js
+++ web/themes/contrib/ensemble/less/js/incentive/incentive.js
@@ -5,8 +5,22 @@
   if($(".field--name-field-argumentaires .field--item").length) {
     moveLinkArgument();
   }
+  if($(".webform-submission-form").length) {
+    moveWebform();
+  } else {
+    $("#custom-form-this-event").hide();
+  }
+
   function moveLinkArgument() {
     $(".field--name-field-argumentaires .field--item a").insertAfter("#pause-cafe").addClass("link-to-arg");
-    $
   }
+
+  function moveWebform() {
+    $(".webform-submission-form").appendTo("#custom-form-this-event").hide();
+    $("#custom-link-this-event").click(function(e) {
+      e.preventDefault();
+      $(".webform-submission-form").slideToggle();
+    });
+  }
+
 })(jQuery);
