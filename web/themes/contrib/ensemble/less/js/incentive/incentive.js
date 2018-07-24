f44f40200ecb6d2b041272f0b781d996a587e204
Yvan Douenel <y.douenel@coopernet.fr>
2018-07-24 07:28:07 +0200

    incentive.js up to better manage form in function of scheduled dates.

--- web/themes/contrib/ensemble/less/js/incentive/incentive.js
+++ web/themes/contrib/ensemble/less/js/incentive/incentive.js
@@ -1,11 +1,11 @@
 (function ($) {
 
-  console.log("dans incentive");
-
   if($(".field--name-field-argumentaires .field--item").length) {
     moveLinkArgument();
   }
-  if($(".webform-submission-form").length) {
+  if($(".webform-submission-form").length
+  && (!($(".webform-submission-form .alert-dismissible").length &&
+    !($("body.user-logged-in").length)))) {
     moveWebform();
   } else {
     $("#custom-form-this-event").hide();
